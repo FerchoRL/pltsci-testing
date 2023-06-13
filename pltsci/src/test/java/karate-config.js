f681c30a049982dfae9a329d1249ca18e072665e
Fernando RL <rlfernando7@gmail.com>
2023-06-13 00:57:39 -0600

    set up karate config

--- pltsci/src/test/java/karate-config.js
+++ pltsci/src/test/java/karate-config.js
@@ -2,17 +2,18 @@ function fn() {
   var env = karate.env; // get system property 'karate.env'
   karate.log('karate.env system property was:', env);
   if (!env) {
-    env = 'dev';
+    env = 'qa';
   }
   var config = {
-    env: env,
-    myVarName: 'someValue'
+    // initialize api url
+    apiUrl: 'http://localhost:8080/v1/'
   }
-  if (env == 'dev') {
-    // customize
-    // e.g. config.foo = 'bar';
-  } else if (env == 'e2e') {
-    // customize
+  if (env == 'qa') {
+    config.apiUrl = "http://localhost:8080/v1/";
+  }
+  else if (env == 'dev') {
+    //In case we have another environment
+    config.apiUrl = "http://localhost:8080/v1/dev/";
   }
   return config;
 }
